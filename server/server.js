a95ec8892ecab80bbfdca0d17b7bfa92cfddfd55
Vinay jain <jainv823@gmail.com>
2025-12-09 16:24:36 +0530

    Create basic Express server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import "dotenv/config"
+import app from "./src/app.js";
+
+
+const PORT = process.env.PORT || 4000
+
+
+app.listen(PORT,() => {
+    console.log(`Server is running on http://localhost:${PORT}...`);
+})
