59f149d6edd1a97a3d2a50c5652c6dbbdd42603f
Vinay jain <jainv823@gmail.com>
2025-12-10 10:09:33 +0530

    feat: Update the task using id

--- server/src/controllers/tasks.controller.js
+++ server/src/controllers/tasks.controller.js
@@ -26,8 +26,28 @@ const getAllTasks = async (req, res) => {
     res.status(200).json({ tasks: data });
   } catch (error) {
     logger.error(`Error featching: ${error.message}`);
-    res.status(400).json({message: error.message})
+    res.status(400).json({ message: error.message });
   }
 };
 
-export { addTask, getAllTasks };
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { name, status } = req.body;
+  if (!name && !status) {
+    res.status(400).json({ error: "Name or status is required" });
+  }
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { name, status },
+      { new: true }
+    );
+    res.status(200).json({ updatedTask: updatedTask });
+    logger.info(`Task updated successfully: ${updatedTask}`);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+    logger.error(`Error updating task: ${error.message}`);
+  }
+};
+
+export { addTask, getAllTasks, updateTask };
--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { addTask, getAllTasks } from "../controllers/tasks.controller.js";
-
+import {
+  addTask,
+  getAllTasks,
+  updateTask,
+} from "../controllers/tasks.controller.js";
 
 const router = Router();
 
-router.route("/").get(getAllTasks)
+router.route("/").get(getAllTasks);
 router.route("/addTask").post(addTask);
+router.route("/updateTask/:id").put(updateTask);
 
 export default router;
